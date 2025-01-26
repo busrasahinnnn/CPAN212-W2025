36255be4dfd28faa88646ab5930a0aa01c1135f0
busrasahinnnn <N01515717@humber.ca>
2025-01-26 14:21:11 -0500

    adding labs repository

---
+++ labs/lab01/lab01-p2.js
@@ -0,0 +1,31 @@
+/*
+home page
+about us
+contact
+login
+register
+details
+search page
+*/
+
+import http from "http"
+import fs from "fs";
+import path from "path";
+
+
+const app = http.createServer((req, res)=>{
+    if(req.url === "/"){
+        res.end("WELCOME TO THE SERVER")
+    }
+    else if(req.url === "/about"){
+        const webpage = fs.readFileSync(path.join("html", "aboutus.html"))
+        res.end(webpage)
+    }
+    else{
+        res.end("Page not found")
+    }
+});
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, ()=>{
+    console.log(`http://localhost:${PORT}`);
+});
